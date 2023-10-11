d455faa432d4a2eee34514ee5d8dfabe511b3448
David-Cyberdyne <68125762+David-Cyberdyne@users.noreply.github.com>
2023-10-11 13:28:01 +0200

    Update PracticeForm.java

--- src/test/java/org/selenium/pages/demoqa/PracticeForm.java
+++ src/test/java/org/selenium/pages/demoqa/PracticeForm.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,7 +12,7 @@ import java.time.Duration;
 
 public class PracticeForm {
 
-    private final WebDriver driver;
+    private WebDriver driver;
 
     public PracticeForm(WebDriver driver) {
         this.driver = driver;
@@ -19,26 +20,27 @@ public class PracticeForm {
 
     //Locators
 
-    private final By firstName = By.id("firstName");
-    private final By lastName = By.id("lastName");
+    private By firstName = By.id("firstName");
+    private By lastName = By.id("lastName");
     private final By male = By.cssSelector("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label");
     private final By female = By.cssSelector("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(2) > label");
     private final By other = By.cssSelector("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(3) > label");
-    private final By mobile = By.id("userNumber");
-    private final By dateOfBirth = By.id("dateOfBirthInput");
-    private final By subjects = By.id("//div[@class='subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']");
-    private final By sports = By.id("hobbies-checkbox-1");
-    private final By reading = By.id("hobbies-checkbox-2");
-    private final By music = By.id("hobbies-checkbox-3");
-    private final By picture = By.id("");
-    private final By currentAddress = By.id("currentAddress");
-    private final By state = By.id("react-select-3-input");
-    private final By city = By.id("react-select-4-input");
+    private By mobile = By.id("userNumber");
+    private By dateOfBirth = By.id("dateOfBirthInput");
+    private By subjects = By.id("subjectsInput");
+    private By sports = By.cssSelector("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label");
+    private By reading = By.cssSelector("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(2) > label");
+    private By music = By.cssSelector("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(3) > label");
+    private By picture = By.id("");
+    private By currentAddress = By.id("currentAddress");
+    private By state = By.id("react-select-3-input");
+    private By city = By.id("react-select-4-input");
 
-    public void waitForElement(By elementLocator, Integer seconds){
+    public WebElement waitForElement(By elementLocator, Integer seconds){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
         WebElement element = driver.findElement(elementLocator);
         wait.until(ExpectedConditions.visibilityOf(element));
+        return element;
     }
     public void typeFirstName(String text){
         waitForElement(firstName, 15);
@@ -51,19 +53,16 @@ public class PracticeForm {
     }
 
     public void selectGender(String gender){
-        WebElement element = null;
-        switch (gender){
+        switch (gender) {
             case "male":
-                element = driver.findElement(male);
+                driver.findElement(male).click();
                 break;
             case "female":
-                element = driver.findElement(female);
+                driver.findElement(female).click();
                 break;
             default:
-                element = driver.findElement(other);
+                driver.findElement(other).click();
         }
-
-        if (!element.isSelected()) element.click();
     }
 
     public void typeMobile(String mobile){
@@ -72,10 +71,54 @@ public class PracticeForm {
     }
 
     public void typeDateOfBirth(String date) throws InterruptedException {
+        WebElement element = driver.findElement(dateOfBirth);
         waitForElement(dateOfBirth, 15);
-        driver.findElement(dateOfBirth).clear();
-        Thread.sleep(5000);
-        driver.findElement(dateOfBirth).sendKeys(date);
-        driver.findElement(dateOfBirth).sendKeys(Keys.ENTER);
+        element.click();
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build().perform();
+        element.sendKeys(date);
+        element.sendKeys(Keys.ENTER);
+    }
+
+    public void typeSubjects(String text){
+        WebElement element = driver.findElement(subjects);
+        waitForElement(subjects, 15);
+        element.sendKeys(text);
+        element.sendKeys(Keys.ENTER);
+    }
+
+    public void selectHobbys(String hobby){
+        switch (hobby) {
+            case "sports":
+                driver.findElement(sports).click();
+                break;
+            case "reading":
+                driver.findElement(reading).click();
+                break;
+            case "music":
+                driver.findElement(music).click();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void typeAddress(String text){
+        waitForElement(currentAddress, 15);
+        driver.findElement(currentAddress).sendKeys(text);
+    }
+
+    public void typeState(String text){
+        WebElement element = driver.findElement(state);
+        waitForElement(state, 15);
+        element.sendKeys(text);
+        element.sendKeys(Keys.ENTER);
+    }
+
+    public void typeCity(String text){
+        WebElement element = driver.findElement(city);
+        waitForElement(city, 15);
+        element.sendKeys(text);
+        element.sendKeys(Keys.ENTER);
     }
 }
